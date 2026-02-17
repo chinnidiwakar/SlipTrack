597e735df56ae820773b9768bbf051dfe6338cf5
Chinni Diwakar <14030364+chinnidiwakar@users.noreply.github.com>
2026-02-17 23:08:39 +0530

    Bump version to 0.50

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         minSdk = 30
         targetSdk = 35
         versionCode = 1
-        versionName = "0.30"
+        versionName = "0.50"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
